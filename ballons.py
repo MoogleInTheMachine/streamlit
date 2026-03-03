0e9bcedf3dca69a96bd93730e327491bad14598b
Rene Zelaya <mooglesthegreat@gmail.com>
2026-03-03 19:52:58 +0100

    Add FART MASTER app with balloon feature

---
+++ ballons.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("FART MASTER 💨")
+st.markdown(
+    """ 
+    Here, you are the  
+
+    **:rainbow[FART MASTER!]**
+    
+    """
+)
+
+if st.button("Send [Fart] balloons!"):
+    st.balloons()
